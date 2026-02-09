070b6ee14061773972d7dc361cefcb69a1c9ad61
YCY <chin-yun.yu@qmul.ac.uk>
2026-02-09 06:34:04 +0000

    refactor: use pyproject.toml as the sole source to install

--- setup.py
+++ setup.py
@@ -1,43 +1,8 @@
 #!/usr/bin/env python3
-# Inspired from https://github.com/kennethreitz/setup.py
+from setuptools import setup
 
-from pathlib import Path
-from setuptools import setup, find_packages
 
-NAME = "auraloss"
-DESCRIPTION = "Audio-focused loss functions in PyTorch"
-URL = "https://github.com/csteinmetz1/auraloss"
-EMAIL = "c.j.steinmetz@qmul.ac.uk"
-AUTHOR = "Christian Steinmetz"
-REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.4.0"
-
-HERE = Path(__file__).parent
-
-try:
-    with open(HERE / "README.md", encoding="utf-8") as f:
-        long_description = "\n" + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=["auraloss"],
-    install_requires=["torch", "numpy"],
-    extras_require={"test": ["resampy"], "all": ["matplotlib", "librosa", "scipy"]},
-    include_package_data=True,
-    license="Apache License 2.0",
-    classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        "Topic :: Multimedia :: Sound/Audio",
-        "Topic :: Scientific/Engineering",
-    ],
-)
+if __name__ == "__main__":
+    # Minimal setup.py so that tooling which executes this file
+    # still works, while `pyproject.toml` is the single source of truth.
+    setup()
